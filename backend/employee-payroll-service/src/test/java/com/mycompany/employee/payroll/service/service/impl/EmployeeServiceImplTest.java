e59469b8ff8fb16bd284f58aeca98e7249dcd21c
ging <ariyawat.nky@gmail.com>
2026-01-06 23:28:08 +0700

    style: fix formatting in EmployeeServiceImplTest

--- backend/employee-payroll-service/src/test/java/com/mycompany/employee/payroll/service/service/impl/EmployeeServiceImplTest.java
+++ backend/employee-payroll-service/src/test/java/com/mycompany/employee/payroll/service/service/impl/EmployeeServiceImplTest.java
@@ -27,8 +27,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class
-EmployeeServiceImplTest {
+class EmployeeServiceImplTest {
 
   @InjectMocks
   private EmployeeServiceImpl employeeService;
